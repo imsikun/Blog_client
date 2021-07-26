293d67eeb40e849af97e2c31c2bbb926e60bebe9
Subh <subhakant@staragrifinance.com>
2021-07-26 19:28:37 +0530

    first commit

---
+++ src/components/topbar/TopBar.js
@@ -0,0 +1,13 @@
+import './topbar.css'
+
+export default function TopBar () {
+  return (
+    <div>
+      <div className='top'>
+        <div className='topLeft'>l</div>
+        <div className='topCenter'>c</div>
+        <div className='topRight'>r</div>
+      </div>
+    </div>
+  )
+}
