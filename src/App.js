fccdfadc1c4b99169203fcc7ba2a0d1637bc3cf5
imsikun <imsikun16@gmail.com>
2021-10-06 15:48:09 +0530

    crated the write page and settings page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import TopBar from './components/topbar/TopBar'
+import Settings from './pages/settings/Settings'
 // import Single from './pages/single/Single'
 import Write from './pages/write/Write'
 // import Home from './pages/Home/Home'
@@ -9,7 +10,7 @@ function App () {
     <div className='App'>
       <TopBar />
       {/* <Single/> */}
-      <Write/>
+      <Settings />
     </div>
   )
 }
