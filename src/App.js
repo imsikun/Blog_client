3b36c01637c922821f98b58fe75d5e1132bf00ed
imsikun <imsikun16@gmail.com>
2021-10-07 15:58:16 +0530

    completed the frontend

--- src/App.js
+++ src/App.js
@@ -1,17 +1,55 @@
 import React from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import TopBar from './components/topbar/TopBar'
-import Settings from './pages/settings/Settings'
-// import Single from './pages/single/Single'
+import Register from './pages/Register/Register'
+import Login from './pages/Login/Login'
 import Write from './pages/write/Write'
-// import Home from './pages/Home/Home'
+import Settings from './pages/settings/Settings'
+import Single from './pages/single/Single'
+import Home from './pages/Home/Home'
+import About from './pages/About/About';
+import Contact from './pages/Contact/Contact';
 
 function App () {
+
+// sudo user to check the register
+const user = false
+
   return (
-    <div className='App'>
+    <Router>
       <TopBar />
-      {/* <Single/> */}
-      <Settings />
-    </div>
+      <Switch>
+        <Route exact path='/'>
+          <Home />
+        </Route>
+        <Route path='/about'>
+          <About />
+        </Route>
+        <Route path='/contact'>
+          <Contact/>
+        </Route>
+        <Route path='/register'>
+          {user ? <Home /> : <Register />}
+        </Route>
+        <Route path='/login'>
+          {user ? <Home /> : <Login />}
+        </Route>
+        <Route path='/write'>
+          {user ? <Write /> : <Register />}
+        </Route>
+        <Route path='/settings'>
+          {user ? <Settings /> : <Register />}
+        </Route>
+        <Route path='/post/:postId'>
+          <Single />
+        </Route>
+      </Switch>
+    </Router>
   )
 }
 
---
+++ src/pages/Login/Login.js
@@ -0,0 +1,19 @@
+import './login.css'
+import { Link } from 'react-router-dom'
+export default function Login() {
+  return (
+    <div className='login'>
+      <span className="loginTitle">Login</span>
+      <form className="loginForm">
+        <label>Email</label>
+        <input type="text"  className='loginInput' placeholder='Enter your mail' />
+        <label>Password</label>
+        <input type="password"  className='loginInput' placeholder='Enter your password' />
+        <button className="loginButton">Login</button>
+        <button className="loginRegisterButton">
+          <Link to='/register' className='link'>Register</Link>
+        </button>
+      </form>
+    </div>
+  )
+}
